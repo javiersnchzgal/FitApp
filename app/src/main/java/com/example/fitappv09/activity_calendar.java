a3bf3f1feeee1dc7ed7b0dd0ecddb5e17413b70a
Javier <javier.sanchez.gallego@gmail.com>
2022-05-06 21:27:58 +0200

    Calendario funcionando

--- app/src/main/java/com/example/fitappv09/activity_calendar.java
+++ app/src/main/java/com/example/fitappv09/activity_calendar.java
@@ -6,13 +6,25 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CalendarView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-public class activity_calendar extends AppCompatActivity implements View.OnClickListener {
+import java.time.LocalDate;
+import java.time.YearMonth;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+
+public class activity_calendar extends AppCompatActivity implements CalendarAdapter.OnItemListener, View.OnClickListener {
+
+    private TextView monthYearText;
+    private RecyclerView calendarRecyclerView;
+    private LocalDate selectedDate;
 
     Button btnCrearEntrenamiento;
-    CalendarView calendarView;
 
     DBHelper db;
     Context context;
@@ -22,27 +34,84 @@ public class activity_calendar extends AppCompatActivity implements View.OnClick
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
+        initWidgets();
+        selectedDate = LocalDate.now();
+        setMonthView();
 
         db = new DBHelper(this);
         context = getApplicationContext();
 
-        btnCrearEntrenamiento = findViewById(R.id.btnCrearEntrenamiento);
-        btnCrearEntrenamiento.setOnClickListener(this);
 
     }
 
+    private void initWidgets() {
+        calendarRecyclerView = findViewById(R.id.calendarRecycler);
+        monthYearText = findViewById(R.id.monthYearTV);
+    }
+
+    private void setMonthView() {
+        monthYearText.setText(monthYearFromDate(selectedDate));
+        ArrayList<String> daysInMonth = daysInMonthArray(selectedDate);
+
+        CalendarAdapter calendarAdapter = new CalendarAdapter(daysInMonth, this);
+        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getApplicationContext(), 7);
+        calendarRecyclerView.setLayoutManager(layoutManager);
+        calendarRecyclerView.setAdapter(calendarAdapter);
+    }
+
+    private ArrayList<String> daysInMonthArray(LocalDate date) {
+        ArrayList<String> daysInMonthArray = new ArrayList<>();
+        YearMonth yearMonth = YearMonth.from(date);
+
+        int daysInMonth = yearMonth.lengthOfMonth();
+
+        LocalDate firstOfMonth = selectedDate.withDayOfMonth(7);
+
+        int dayOfWeek = firstOfMonth.getDayOfWeek().getValue();
+
+        for (int i = 1; i<=42; i++ ){
+            if (i <= dayOfWeek || i > daysInMonth + dayOfWeek){
+                daysInMonthArray.add("");
+            } else {
+                daysInMonthArray.add(String.valueOf(i - dayOfWeek));
+            }
+        }
+        return daysInMonthArray;
+    }
+
+    private String monthYearFromDate(LocalDate date){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMMM yyyy");
+        return date.format(formatter);
+    }
 
     @Override
     public void onClick(View v) {
-
+/*
         switch (v.getId()){
             case R.id.btnCrearEntrenamiento:
-                    calendarView.getDate();
 
                     intent = new Intent(this, activity_exercises.class);
                     startActivity(intent);
                 break;
 
+        }*/
+    }
+
+    public void previousMonthAction(View view){
+        selectedDate = selectedDate.minusMonths(1);
+        setMonthView();
+    }
+
+    public void nextMonthAction(View view){
+        selectedDate = selectedDate.plusMonths(1);
+        setMonthView();
+    }
+
+    @Override
+    public void onItemClick(int position, String dayText) {
+        if (!dayText.equals("")){
+            String message = "Fecha seleccionada " + dayText + " " + monthYearFromDate(selectedDate);
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
         }
     }
 }
